c7f22a7b10efaffbc02f05494be1940bfdd61114
Darrel <nick94@teamoconnor.net>
2018-04-25 14:51:41 -0500

    testing

--- NewTTT.cpp
+++ NewTTT.cpp
@@ -4,26 +4,69 @@
 #include<cstdlib>
 #include<cmath>
 
-//*********************************************************
-//    int board[3][3] = {{7, 8, 9}, {4, 5, 6}, {1, 2, 3}} *
-//    <array name>[row][column]                           *
-//    this is the board:                                  *
-//                                                        *
-//                  columns                               *
-//                  [0 1 2]                               *
-//    r      [0]    7 8 9                                 *
-//    o      [1]    4 5 6                                 *
-//    W      [2]    1 2 3                                 *
-//*********************************************************
+//* * * * * * * * * * * * * * * * *
+//    <array name>[row][column]   *
+//    this is the board:          *
+//                                *
+//                columns         *
+//                [0 1 2]         *
+//    r    [0]    7 8 9           *
+//    o    [1]    4 5 6           *
+//    W    [2]    1 2 3           *
+//* * * * * * * * * * * * * * * * *
 
 using namespace std;
 
-int boardOutput(char XnO[3][3]);
-int endGame(char board2[3][3]);
+int rounds();
+void drawBoard(char board2[3][3]);
+void xInput(short x);
+void oInput(short o);
+//void boardOutput(char XnO[3][3]);
+void testBoard(char board3[3][3]);
 
-int main(int victor){
+int main(){
+  short x, o;
+  char board[3][3] = {{'e', 'e', 'e'}, {'e', 'e', 'e'}, {'e', 'e', 'e'}};
+  int rounds = rounds();
+
+  for(x = 1; x <= rounds; x++){
+    bool g = true;
+    while(g != false){
+      drawBoard(board);
+      xInput(x);
+      drawBoard();
+      oInput(o);
+      testBoard();
+    }
+  }
+}
+
+int rounds(){
+  int rnds;
+  do{
+    cout << "How many rounds do you want to play for?";
+    cin >> rnds;
+  }while(rnds <= 0);
+  return rnds;
+}
+
+void drawBoard(char board[3][3]){
+  cout << "\n " << endl;
+  cout << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+  cout << "-----------" << endl;
+  cout << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+  cout << "-----------" << endl;
+  cout << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+  cout << "\n " << endl; 
+}
+
+
+
+
+
+/*
+int main(){
   srand(time(0));
-  bool matchActive = true, moveAuthentication = false;
   short input;
   char board[3][3] = {{'e', 'e', 'e'}, {'e', 'e', 'e'}, {'e', 'e', 'e'}};
   victor = 0;
@@ -138,8 +181,8 @@ int main(int victor){
   return 0;
 }
 
-int endGame(short board[9]){
-  if(board[0] == 1 && board[1] == 1 && board[2] == 1)
+int testBoard(short board[9]){
+  if(board[0][0] == 1 && board[1] == 1 && board[2] == 1)
     main(1);
   if(board[3] == 1 && board[4] == 1 && board[5] == 1)
     main(1);
@@ -183,3 +226,4 @@ int boardOutput(short XnO[9]){
   cout << XnO[6] << " | " << XnO[7] << " | " << XnO[8] << endl;
   cout << "\n(Anything with a zero is an empty space)" << endl;
 }
+*/
