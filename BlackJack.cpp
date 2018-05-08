42eaea10f0d16b2b90090321427de1faa1da89df
Darrel <nick94@teamoconnor.net>
2018-05-08 15:02:29 -0500

    same

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -12,17 +12,31 @@ int main(){
   int rounds = howManyRounds(); //how many rounds they want to play BJ
 
   for(int x = 1; x <= rounds; x++){//repeats for the amount that you entered
-    int card = ((rand() % 10)+1);//assigns the card number to 1-13 !!ONLY IN THIS FOR LOOP!!
+    int gameTrig = 1;
 
-    if(card == 1){
+    while(gameTrig == 1){
+      int card = ((rand() % 10)+1);//assigns the card number to 1-13 !!ONLY IN THIS LOOP!!
+
+      if(card == 1){
+        do{
+          cout << "You got an Ace! Do you want that to be a 1 or 11? " << endl;
+          cin >> card;
+        }while(card != 1 || card != 11);
+      }
+
+      cardGraphic(card);
+
+      char addC = ' ';
+
       do{
-        cout << "You got an Ace! Do you want that to be a 1 or 11? " << endl;
-        cin >> card;
-      }while(card != 1 || card != 11);
-    }
+        cout << "You have a " << card << "do you want to add another card or fold? [A]dd or [F]old: " << endl;
+        cin >> addC;
+      }while(addC != 'A' || addC != 'a' || addC != 'F' || addC != 'f');
 
-  }//End of the game loop (when the user folds or is over 21 - it loops while they still have rounds to play)
 
+
+    }//End of While Loop (when the user folds or is over 21 it ends)
+  }//End of the game loop (it loops while they still have rounds to play)
   return 0;
 }
 
