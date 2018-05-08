3e092182ef127edbfc2125ef0bf8f344f4bae695
SlamJamCentral <nick94@teamoconnor.net>
2018-05-07 20:12:14 -0500

    trying to get off the ground

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -4,7 +4,7 @@
 #include<ctime>
 using namespace std;
 
-void cards(short number){
+void cards(short number){//Display the card number
   cout << "_________\n";
   cout << "|" << setw(9) << "|\n";
   cout << "|" << setw(9) << "|\n";
@@ -15,55 +15,53 @@ void cards(short number){
 }
 
 
-int setup(){
-  short numEntry = 5;
-  char game;
+int howManyRounds(){
+  short rounds;
 
-  cout << "Do you want to play some Black Jack?\n";
-  cin >> game;
+  cout << "How many rounds of BlackJack do you want to play?" << endl;
+  cin >> rounds;
 
-  //Whether they want to play BJ
-  switch(game){
-    case 'n':
-    case 'N': return 0;
-    break;
-    default: cout << "Starting...\n";
-  }
+  return rounds;
 }
 
 
 int main(){
-  setup();
-  cout << "\n";
+  int rounds = howManyRounds(); //how many rounds they want to play BJ
   srand(time(0));
-  short number, totalNum; //card number -- total number of the cards
-  char addVar; //char for adding cards
-  bool addC = false, gameFlag = false; //Bool to add a Card -- bool to end the game
 
-  while(gameFlag == false){
-
-  do{
-    number = rand();
-    number = number % 11;
-    ++number;
-  }while(number <= 0 || number >= 12);
-
-  if(number == 1){
-    cout << "You got an ace! Do you want that to be 1 or 11?";
-    cin >> number;
+  for(int x = 1; x <= rounds; x++){//repeats for the amount that you entered
+    int card = (rand() % 14)
   }
-
-  cards(number);
-
-  cout << "\nDo you want to add another card? (y/n) ";
-  cin >> addVar;
-
-  switch(addVar){
-    case 'y':
-    case 'Y':
-
-  }
-
-}
   return 0;
 }
+
+/*
+setup();
+cout << "\n";
+srand(time(0));
+short number, totalNum; //card number -- total number of the cards
+char addVar; //char for adding cards
+bool addC = false, gameFlag = false; //Bool to add a Card -- bool to end the game
+
+while(gameFlag == false){
+
+do{
+  number = rand();
+  number = number % 11;
+  ++number;
+}while(number <= 0 || number >= 12);
+
+if(number == 1){
+  cout << "You got an ace! Do you want that to be 1 or 11?";
+  cin >> number;
+}
+
+cards(number);
+
+cout << "\nDo you want to add another card? (y/n) ";
+cin >> addVar;
+
+switch(addVar){
+  case 'y':
+  case 'Y':
+*/
