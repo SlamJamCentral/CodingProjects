922feac0b0fcb0344a89d97b921ca85a0df394ab
Programming <Programming@Nicks-MacBook-Pro.local>
2018-05-16 17:11:01 -0500

    WARNING: WILL DESTROY YOUR RAM

--- RPNG.cpp
+++ RPNG.cpp
@@ -9,10 +9,10 @@ int howManyRounds(){
   int rounds;
 
   do{
-    cout << "Be reasonable, you don't need anything over 100 or less than 0" << endl;
-    cout << "How many numbers would you like to generate? ";
+    cout << "This takes a long time. Start at your own risk." << endl;
+    cout << "How many numbers would you like to generate? " << endl;
     cin >> rounds;
-  }while(rounds < 1 || rounds > 100);
+  }while(rounds < 1 || rounds > 5);
 
   return rounds;
 }
@@ -24,6 +24,7 @@ int numberGenerator(){
 
   do{
     number = rand();
+    cout << "Testing: " << number << endl;
 
     if((number % 2) == 0)
       prime = false;
@@ -47,13 +48,22 @@ int numberGenerator(){
 }
 
 int main(){
-  int numbers;
+  int numbers, startTime, endTime, totTime;
   int repeats = howManyRounds();
+
+  startTime = time(0);
+  cout << "Starting time: " << startTime << endl;
 
   for(int x = 1; x <= repeats; x++){
     numbers = numberGenerator();
-    cout << numbers << endl;
+    cout << "Prime Number: " << numbers << endl;
   }
+
+  endTime = time(0);
+  cout << "Ending time: " << endTime << endl;
+
+  totTime = endTime - startTime;
+  cout << "Total elapsed time: " << totTime << endl;
 
   return 0;
 }
@@ -68,4 +78,6 @@ for(int x = 0; x <= howMany; x++){
   number = rand();
   cout << number << endl;
 }
+15265050101
+1526505012
 */
