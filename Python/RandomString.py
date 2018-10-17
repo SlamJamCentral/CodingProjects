3be1705646ae91eb2f8b058c0fb8b333cf10eaa4
Programming <Programming@Nicks-MacBook-Pro.local>
2018-10-16 21:41:28 -0500

    Python makes no sense anymore

---
+++ Python/RandomString.py
@@ -0,0 +1,8 @@
+import string
+import random
+
+var1 = input("How many times would you like to generate a random string? ")
+
+for x in range(var1):
+    var2 = input("How many characters would you like the string to be? ")
+    print ''.join(random.choice(string.ascii_uppercase+string.ascii_lowercase+string.digits) for _ in range(var2))
